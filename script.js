c0525db56ed8919bbb38836cbcfba91b06dc3c35
sahilpatel0oo7 <109283616+sahilpatel0oo7@users.noreply.github.com>
2023-03-28 01:16:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function b1() {
     
     if (flash == 1) { 
         document.getElementById("b1").value = "X";
+        document.getElementById("b1").disabled = true;
         flash = 0
+
 
     }
 
     else {document.getElementById("b1").value = "0";
+    document.getElementById("b1").disabled = true;
     flash = 1}
 
 }
@@ -16,9 +19,11 @@ function b2() {
     
     if (flash == 1) { 
         document.getElementById("b2").value = "X"
+        document.getElementById("b2").disabled = true;
      flash = 0}
 
     else {document.getElementById("b2").value = "0";
+    document.getElementById("b2").disabled = true;
     flash = 1}
 
 }
@@ -26,9 +31,11 @@ function b3() {
     
     if (flash == 1) { 
         document.getElementById("b3").value = "X"
+        document.getElementById("b3").disabled = true;
      flash = 0}
 
     else {document.getElementById("b3").value = "0";
+    document.getElementById("b3").disabled = true;
     flash = 1}
 
 }
@@ -36,10 +43,11 @@ function b4() {
     
     if (flash == 1) { 
         document.getElementById("b4").value = "X"
+        document.getElementById("b4").disabled = true;
      flash = 0}
 
     else {document.getElementById("b4").value = "0";
-     
+     document.getElementById("b4").disabled = true;
     flash = 1}
 
 }
@@ -47,9 +55,11 @@ function b5() {
     
     if (flash == 1) { 
         document.getElementById("b5").value = "X"
+        document.getElementById("b5").disabled = true;
      flash = 0}
 
     else {document.getElementById("b5").value = "0";
+    document.getElementById("b5").disabled = true;
     flash = 1}
 
 }
@@ -57,9 +67,11 @@ function b6() {
     
     if (flash == 1) { 
         document.getElementById("b6").value = "X"
+        document.getElementById("b6").disabled = true;
      flash = 0}
 
     else {document.getElementById("b6").value = "0";
+    document.getElementById("b6").disabled = true;
     flash = 1}
 
 }
@@ -67,9 +79,11 @@ function b7() {
     
     if (flash == 1) { 
         document.getElementById("b7").value = "X"
+        document.getElementById("b7").disabled = true;
      flash = 0}
 
     else {document.getElementById("b7").value = "0";
+    document.getElementById("b7").disabled = true;
     flash = 1}
 
 }
@@ -77,9 +91,11 @@ function b8() {
     
     if (flash == 1) { 
         document.getElementById("b8").value = "X"
+        document.getElementById("b8").disabled = true;
      flash = 0}
 
     else {document.getElementById("b8").value = "0";
+    document.getElementById("b8").disabled = true;
     flash = 1}
 
 }
@@ -87,22 +103,26 @@ function b9() {
     
     if (flash == 1) { 
         document.getElementById("b9").value = "X"
+        document.getElementById("b9").disabled = true;
      flash = 0}
 
     else {document.getElementById("b9").value = "0";
+    document.getElementById("b9").disabled = true;
     flash = 1}
 
 }
 
 function refresh(){
-    document.getElementById("b1").value = "";
-    document.getElementById("b2").value = "";
-    document.getElementById("b3").value = "";
-    document.getElementById("b4").value = "";
-    document.getElementById("b5").value = "";
-    document.getElementById("b6").value = "";
-    document.getElementById("b7").value = "";
-    document.getElementById("b8").value = "";
-    document.getElementById("b9").value = "";
+    location.reload();
+    document.getElementById("b1").value = ""; disabled = false;
+    document.getElementById("b2").value = ""; disabled = false;
+    document.getElementById("b3").value = ""; disabled = false;
+    document.getElementById("b4").value = ""; disabled = false;
+    document.getElementById("b5").value = ""; disabled = false;
+    document.getElementById("b6").value = ""; disabled = false;
+    document.getElementById("b7").value = ""; disabled = false;
+    document.getElementById("b8").value = ""; disabled = false;
+    document.getElementById("b9").value = ""; disabled = false;
+    
    
 }
